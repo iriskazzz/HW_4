380a913a48f5869c212be32324c4123a9227e0c4
Irina Zolina <JaredLeto@@2317>
2023-09-27 23:15:10 +0300

    delete unused import

--- src/test/java/SelenideRepository.java
+++ src/test/java/SelenideRepository.java
@@ -2,7 +2,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
